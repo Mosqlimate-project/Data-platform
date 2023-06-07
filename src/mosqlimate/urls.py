192b57d7a53420af2fc54914a817a204c93775be
Luã Bida Vacaro <luabidaa@gmail.com>
2023-06-07 16:06:54 -0300

    feat(containers): add postgres container

--- src/mosqlimate/urls.py
+++ src/mosqlimate/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from .api import api
 
 urlpatterns = [
-    path('api/', api.urls),
-    path('admin/', admin.site.urls),
+    path("api/", api.urls),
+    path("admin/", admin.site.urls),
 ]
