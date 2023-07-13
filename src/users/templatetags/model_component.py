260bdda4c03fcce823a817aee77fd357e45d9abd
Luã Bida Vacaro <luabidaa@gmail.com>
2023-07-13 18:11:39 -0300

    Creating the base of the Model Box on user Profile

--- src/users/templatetags/model_component.py
+++ src/users/templatetags/model_component.py
@@ -1,10 +1,10 @@
 from django import template
-from registry.models import Model
 
 register = template.Library()
 
 
-@register.inclusion_tag("users/components/model.html", takes_context=False)
-def model_component(model: Model):
-    context = {"model_name": model.name}
+@register.inclusion_tag("users/components/models-box.html", takes_context=True)
+def models_box(context):
+    models = context.get("user_models")
+    context = {"models": models}
     return context
--- src/users/templatetags/prediction_component.py
+++ src/users/templatetags/prediction_component.py
@@ -4,7 +4,7 @@ from registry.models import Prediction
 register = template.Library()
 
 
-@register.inclusion_tag("users/components/prediction.html", takes_context=False)
-def prediction_component(prediction: Prediction):
-    context = {"predict_date": prediction.predict_date}
+@register.inclusion_tag("users/components/prediction-item.html", takes_context=False)
+def prediction_item(prediction: Prediction):
+    context = {"prediction": prediction}
     return context
