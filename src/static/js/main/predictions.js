fe3c756c9c75a6b60be0e7f18e342349334211b2
Luã Bida Vacaro <luabidaa@gmail.com>
2023-07-28 10:45:18 -0300

    Add 404 not found when no predictions were found

--- src/static/js/main/predictions.js
+++ src/static/js/main/predictions.js
@@ -1,13 +1,23 @@
-function resetPageInput() {
-  $("input[name='page']").val(1);
-}
-
 function clearInput(inputName) {
   var inputElement = document.querySelector(`input[name='${inputName}']`);
   if (inputElement) {
     inputElement.value = '';
-  }
+  };
 }
+
+function clearAllInputs() {
+  const inputs = document.querySelectorAll('.form-control');
+  inputs.forEach((input) => {
+    if (input.name !== 'page' && input.name !== 'per_page') {
+      input.value = '';
+    }
+  });
+}
+
+function resetPageInput() {
+  $("input[name='page']").val(1);
+}
+
 
 document.addEventListener('DOMContentLoaded', function() {
   const modelCollapse = document.getElementById('modelCollapse');
