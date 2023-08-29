33e0acdb8422c37d41069a8f35fe31cfdfeed834
Luã Bida Vacaro <luabidaa@gmail.com>
2023-08-29 14:52:14 -0300

    Implement ImplementationLanguage on Model

--- src/registry/models.py
+++ src/registry/models.py
@@ -43,8 +43,10 @@ class Model(models.Model):
     )
     name = models.CharField(max_length=100, null=False, blank=False)
     description = models.TextField(max_length=255, null=True, blank=True)
-    repository = models.URLField(max_length=150, null=False, blank=False)
-    implementation_language = models.CharField(max_length=100, null=False, blank=False)
+    repository = models.CharField(max_length=100, null=False, blank=False)
+    implementation_language = models.ForeignKey(
+        ImplementationLanguage, on_delete=models.PROTECT, null=False, blank=False
+    )
     type = models.CharField(max_length=100, null=False, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
