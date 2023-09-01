092b17701af16a8f7b4d7ef66a16632cc203fb12
Luã Bida Vacaro <luabidaa@gmail.com>
2023-09-01 14:36:20 -0300

    Change search form to list implementation languages instead of having to type it

--- src/static/js/main/predictions.js
+++ src/static/js/main/predictions.js
@@ -13,6 +13,9 @@ function clearAllInputs() {
     if (input.name !== 'page' && input.name !== 'per_page') {
       input.value = '';
     }
+    if (input.name == 'implementation_language') {
+      input.selectpicker('refresh')
+    }
   });
 }
 
