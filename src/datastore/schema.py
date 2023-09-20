d57e4b88dcf4fc09ea86a9323e2c635258ffd723
Luã Bida Vacaro <luabidaa@gmail.com>
2023-09-20 14:45:54 -0300

    Make API result fields optional (in case of NULL values)

--- src/datastore/schema.py
+++ src/datastore/schema.py
@@ -1,37 +1,38 @@
+from typing import Optional
 from datetime import date
 from ninja import Field, Schema, FilterSchema
 
 
 class HistoricoAlertaSchema(Schema):
     data_iniSE: date
-    SE: int
-    casos_est: float
-    casos_est_min: int
-    casos_est_max: int
-    casos: int
-    municipio_geocodigo: int
-    p_rt1: float
-    p_inc100k: float
-    Localidade_id: int
-    nivel: int
-    id: int
-    versao_modelo: str
-    municipio_nome: str
-    pop: float
-    tempmin: float
-    umidmax: float
-    receptivo: int
-    transmissao: int
-    nivel_inc: int
-    umidmed: float
-    umidmin: float
-    tempmed: float
-    tempmax: float
-    casprov: int
-    casprov_est: float
-    casprov_est_min: int
-    casprov_est_max: int
-    casconf: int
+    SE: Optional[int]
+    casos_est: Optional[float]
+    casos_est_min: Optional[int]
+    casos_est_max: Optional[int]
+    casos: Optional[int]
+    municipio_geocodigo: Optional[int]
+    p_rt1: Optional[float]
+    p_inc100k: Optional[float]
+    Localidade_id: Optional[int]
+    nivel: Optional[int]
+    id: Optional[int]
+    versao_modelo: Optional[str]
+    municipio_nome: Optional[str]
+    pop: Optional[float]
+    tempmin: Optional[float]
+    umidmax: Optional[float]
+    receptivo: Optional[int]
+    transmissao: Optional[int]
+    nivel_inc: Optional[int]
+    umidmed: Optional[float]
+    umidmin: Optional[float]
+    tempmed: Optional[float]
+    tempmax: Optional[float]
+    casprov: Optional[int]
+    casprov_est: Optional[float]
+    casprov_est_min: Optional[int]
+    casprov_est_max: Optional[int]
+    casconf: Optional[int]
 
 
 class HistoricoAlertaFilterSchema(FilterSchema):
