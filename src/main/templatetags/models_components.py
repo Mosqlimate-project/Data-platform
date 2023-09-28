14876198ffcd0aaa8f2233a93c2e3de62e0e1600
Luã Bida Vacaro <luabidaa@gmail.com>
2023-09-28 13:45:13 -0300

    include edit button on models-list

--- src/main/templatetags/models_components.py
+++ src/main/templatetags/models_components.py
@@ -5,6 +5,11 @@ from registry.models import Model
 register = template.Library()
 
 
-@register.inclusion_tag("main/components/models-list.html", takes_context=False)
-def models_list(models: list[Model]):
-    return {"models": models}
+@register.inclusion_tag("main/components/models-list.html", takes_context=True)
+def models_list(context, models: list[Model]):
+    user = context.request.user
+    context = {
+        "user": user,
+        "models": models,
+    }
+    return context
