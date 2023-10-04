55e6c088a5503652bf81fac2dbf635867ca3b6c6
Sandro Loch <es.loch@gmail.com>
2023-10-04 13:40:59 -0300

    chore(vis): Add min and max of total cases

--- src/main/templatetags/vis_echarts_components.py
+++ src/main/templatetags/vis_echarts_components.py
@@ -7,6 +7,12 @@ register = template.Library()
 @register.inclusion_tag("main/components/vis-echarts.html", takes_context=True)
 def vis_echarts(context):
     br_data = get_data()
+
+    # Ordenar a lista br_data pelo valor em ordem decrescente
+    br_data.sort(key=lambda x: x["value"], reverse=True)
+
+    context["min_total_cases"] = min(item["value"] for item in br_data)
+    context["max_total_cases"] = max(item["value"] for item in br_data)
     context["br_data"] = br_data
 
     return context
