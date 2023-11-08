badcb2686cb662827e6418e6073e05861485e2f9
Sandro Loch <es.loch@gmail.com>
2023-11-08 15:47:08 -0300

    feat(tests): Include JSON test data for API testing

--- src/registry/tests/test_api.py
+++ src/registry/tests/test_api.py
@@ -1,10 +1,16 @@
+import json
+from pathlib import Path
+
 from django.test import TestCase
 from django.http import HttpRequest
 from ninja import Router
+
 from registry.models import Author, Model, Prediction, ImplementationLanguage
 from users.models import CustomUser
 from registry.api import PredictionIn, create_prediction
 
+
+app_dir = Path(__file__).parent.parent
 
 router = Router()
 
@@ -14,6 +20,10 @@ router = Router()
 )
 class TestCreatePrediction(TestCase):
     def setUp(self):
+        data_path = app_dir / "tests/data/prediction.test.json"
+        with open(data_path, "r") as file:
+            self.data = json.load(file)
+
         user, created = CustomUser.objects.get_or_create(username="esloch")
 
         language = ImplementationLanguage.objects.create(language="MosqLang")
@@ -25,6 +35,7 @@ class TestCreatePrediction(TestCase):
         )
 
     def test_create_prediction(self):
+        # breakpoint()
         payload = PredictionIn(
             model=self.model.pk,
             description="Test description",
@@ -32,7 +43,7 @@ class TestCreatePrediction(TestCase):
             value=42.0,
             commit="76eb927067cf54ae52da53503a14519d78a37da8",
             predict_date="2023-11-08",
-            prediction="data.json",
+            prediction=self.data,
         )
 
         request = HttpRequest()
@@ -58,7 +69,7 @@ class TestCreatePrediction(TestCase):
             value=42.0,
             commit="76eb927067cf54ae52da53503a14519d78a37da8",
             predict_date="2023-11-08",
-            prediction="data.json",
+            prediction=self.data,
         )
 
         request = HttpRequest()
