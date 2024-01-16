169998830328b52fc00d3d6bb1ee200041b95019
Luã Bida Vacaro <luabidaa@gmail.com>
2024-01-16 15:33:25 -0300

    Include /api/city_info/ to extract mun info from geocode

--- src/vis/views.py
+++ src/vis/views.py
@@ -1,7 +1,6 @@
 import os
 import json
 from typing import Union
-from collections import defaultdict
 
 from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
@@ -18,70 +17,24 @@ class DashboardView(View):
     def get(self, request):
         context = {}
 
-        # model_id = request.GET.get("model")
-        # prediction_id = request.GET.get("predict")
-
-        # all_models = Model.objects.all()
-
-        # selected_series: Literal["spatial", "time"] = "time"
-        # selected_adm_level: Literal[0, 1, 2, 3] = 2
-        # selected_disease: Literal["dengue", "zika", "chikungunya"] = "dengue"
-        # selected_geocode: int = None
-        # selected_prediction: int = None
-
-        # line_charts_default_items = []
-
-        # if model_id:
-        #     model = get_object_or_404(Model, pk=int(model_id))
-        #     selected_series = model.type
-        #     selected_adm_level = model.ADM_level
-        #     selected_disease = model.disease
-        #     predictions = Prediction.objects.filter(model=model)
-        #     geocodes = list(
-        #         set(p.adm_2_geocode for p in predictions if p.adm_2_geocode)
-        #     )
-        #     selected_geocode = geocodes[0]
-
-        # if prediction_id:
-        #     prediction = get_object_or_404(Prediction, pk=int(prediction_id))
-        #     selected_series = prediction.model.type
-        #     selected_adm_level = model.ADM_level
-        #     selected_disease = model.disease
-        #     if prediction.adm_2_geocode:
-        #         selected_geocode = prediction.adm_2_geocode
-        #     selected_prediction = prediction.id
-
-        #     line_charts_default_items.append(f"predict={prediction.id}")
-
-        # context["compatibilities"] = generate_models_compatibility_info(
-        #     all_models, json_return=True
-        # )
-
-        # context["available_adm_levels"] = get_available_adm_levels(all_models)
-        # context["available_diseases"] = get_available_diseases(all_models)
-        # context["available_geocodes"] = get_available_adm_2_geocodes(
-        #     all_models
-        # )
-
-        # context["selected_series"] = selected_series
-        # context["selected_adm_level"] = selected_adm_level
-        # context["selected_disease"] = selected_disease
-        # context["selected_geocode"] = selected_geocode
-        # context["selected_prediction"] = selected_prediction
-
-        # context["line_charts_default_uri"] = "?" + "&".join(
-        #     line_charts_default_items
-        # )
-
         models = Model.objects.all()
         predictions = Prediction.objects.filter(visualizable=True)
 
-        context["predictions"] = dict(
+        predictions_data = list(
             zip(
                 list(predictions.values_list("id", flat=True)),
+                list(predictions.values_list("model__name", flat=True)),
                 list(predictions.values_list("metadata", flat=True)),
+                [
+                    g if g else ""
+                    for g in predictions.values_list(
+                        "adm_2_geocode", flat=True
+                    )
+                ],
             )
         )
+
+        context["predictions"] = predictions_data
 
         model_types = set()
         output_formats = set()
@@ -244,58 +197,3 @@ def get_geocode_info(request, geocode: Union[str, int]):
         return JsonResponse(data)
     else:
         return JsonResponse({"error": "Geocode not found"}, status=404)
-
-
-def generate_models_compatibility_info(
-    models: list[Model], json_return: bool = False
-) -> dict:
-    info = defaultdict()
-    for _type in Model.Types:
-        info[str(_type)] = defaultdict()
-
-    for disease in Model.Diseases:
-        for _type in info:
-            info[_type][str(disease)] = defaultdict()
-
-    for adm_level in Model.ADM_levels:
-        for _type in info:
-            for disease in info[_type]:
-                if adm_level == 2:
-                    info[_type][disease][adm_level.value] = defaultdict(
-                        lambda: defaultdict(list)
-                    )
-                else:
-                    info[_type][disease][adm_level.value] = defaultdict(list)
-
-    for model in models:
-        predictions = Prediction.objects.filter(model=model)
-        for prediction in predictions:
-            if not model.type:
-                continue
-
-            if not model.disease:
-                continue
-
-            if model.ADM_level == model.ADM_levels.MUNICIPALITY:
-                if prediction.adm_2_geocode:
-                    info[model.type][model.disease][model.ADM_level][
-                        prediction.adm_2_geocode
-                    ][model.id].append(prediction.id)
-            else:
-                info[model.type][model.disease][model.ADM_level][
-                    model.id
-                ].append(prediction.id)
-
-    info = json.dumps(info, cls=SetToListEncoder)
-
-    if not json_return:
-        return json.loads(info)
-
-    return info
-
-
-class SetToListEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, set):
-            return list(obj)
-        return super().default(obj)
