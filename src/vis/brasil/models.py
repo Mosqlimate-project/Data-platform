2cb73af1d9c8d6006df0730214f7fd4767cc7bb1
Luã Bida Vacaro <luabidaa@gmail.com>
2024-03-06 14:30:27 -0300

    Prepare migrations

--- src/vis/brasil/models.py
+++ src/vis/brasil/models.py
@@ -3,70 +3,75 @@ from django.contrib.gis.db import models as geomodels
 
 
 class GeoMacroregion(geomodels.Model):
-    geocode = geomodels.IntegerField(
-        primary_key=True, max_length=1, unique=True
-    )
+    geocode = geomodels.CharField(primary_key=True, max_length=1, unique=True)
     geometry = geomodels.MultiPolygonField()
+
+    class Meta:
+        app_label = "vis"
 
 
 class GeoState(geomodels.Model):
-    geocode = geomodels.IntegerField(
-        primary_key=True, max_length=2, unique=True
-    )
+    geocode = geomodels.CharField(primary_key=True, max_length=2, unique=True)
     geometry = geomodels.MultiPolygonField()
+
+    class Meta:
+        app_label = "vis"
 
 
 class GeoMesoregion(geomodels.Model):
-    geocode = geomodels.IntegerField(
-        primary_key=True, max_length=4, unique=True
-    )
+    geocode = geomodels.CharField(primary_key=True, max_length=4, unique=True)
     geometry = geomodels.MultiPolygonField()
+
+    class Meta:
+        app_label = "vis"
 
 
 class GeoMicroregion(geomodels.Model):
-    geocode = geomodels.IntegerField(
-        primary_key=True, max_length=5, unique=True
-    )
+    geocode = geomodels.CharField(primary_key=True, max_length=5, unique=True)
     geometry = geomodels.MultiPolygonField()
+
+    class Meta:
+        app_label = "vis"
 
 
 class GeoCity(geomodels.Model):
-    geocode = geomodels.IntegerField(
-        primary_key=True, max_length=7, unique=True
-    )
+    geocode = geomodels.CharField(primary_key=True, max_length=7, unique=True)
     geometry = geomodels.MultiPolygonField()
+
+    class Meta:
+        app_label = "vis"
 
 
 class GeoMacroSaude(geomodels.Model):
-    geocode = geomodels.IntegerField(
-        primary_key=True, max_length=4, unique=True
-    )
+    geocode = geomodels.CharField(primary_key=True, max_length=4, unique=True)
     geometry = geomodels.MultiPolygonField()
+
+    class Meta:
+        app_label = "vis"
 
 
 class Macroregion(models.Model):
     """
-    geocode: 1 to 5
+    geocode: "1" to "5"
     """
 
-    geocode = geomodels.IntegerField(
-        primary_key=True, max_length=1, unique=True
-    )
+    geocode = geomodels.CharField(primary_key=True, max_length=1, unique=True)
     name = models.CharField(null=False)
     geo = models.ForeignKey(GeoMacroregion, on_delete=models.PROTECT)
 
     def __str__(self):
         return f"{self.name}"
 
+    class Meta:
+        app_label = "vis"
+
 
 class State(models.Model):
     """
-    geocode: UF geocode
+    geocode: UF geocode (example: "33")
     """
 
-    geocode = geomodels.IntegerField(
-        primary_key=True, max_length=2, unique=True
-    )
+    geocode = geomodels.CharField(primary_key=True, max_length=2, unique=True)
     name = models.CharField(null=False)
     uf = models.CharField(max_length=2, null=False)
     macroregion = models.ForeignKey(Macroregion, on_delete=models.PROTECT)
@@ -75,15 +80,16 @@ class State(models.Model):
     def __str__(self):
         return f"{self.name}"
 
+    class Meta:
+        app_label = "vis"
+
 
 class Mesoregion(models.Model):
     """
-    geocode: UF geocode + inner code (format: 01)
+    geocode: UF geocode + inner code (example: "3301")
     """
 
-    geocode = geomodels.IntegerField(
-        primary_key=True, max_length=4, unique=True
-    )
+    geocode = geomodels.CharField(primary_key=True, max_length=4, unique=True)
     name = models.CharField(null=False)
     state = models.ForeignKey(State, on_delete=models.PROTECT)
     geo = models.ForeignKey(GeoMesoregion, on_delete=models.PROTECT)
@@ -91,15 +97,16 @@ class Mesoregion(models.Model):
     def __str__(self):
         return f"{self.name}"
 
+    class Meta:
+        app_label = "vis"
+
 
 class Microregion(models.Model):
     """
-    geocode: UF geocode + inner code (format: 001)
+    geocode: UF geocode + inner code (example: "33001")
     """
 
-    geocode = geomodels.IntegerField(
-        primary_key=True, max_length=5, unique=True
-    )
+    geocode = geomodels.CharField(primary_key=True, max_length=5, unique=True)
     name = models.CharField(null=False)
     state = models.ForeignKey(State, on_delete=models.PROTECT)
     geo = models.ForeignKey(GeoMicroregion, on_delete=models.PROTECT)
@@ -107,21 +114,25 @@ class Microregion(models.Model):
     def __str__(self):
         return f"{self.name}"
 
+    class Meta:
+        app_label = "vis"
+
 
 class City(models.Model):
     """
     geocode: https://www.ibge.gov.br/explica/codigos-dos-municipios.php
     """
 
-    geocode = geomodels.IntegerField(
-        primary_key=True, max_length=7, unique=True
-    )
+    geocode = geomodels.CharField(primary_key=True, max_length=7, unique=True)
     name = models.CharField(null=False)
     state = models.ForeignKey(State, on_delete=models.PROTECT)
     geo = models.ForeignKey(GeoState, on_delete=models.PROTECT)
 
     def __str__(self):
         return f"{self.name}"
+
+    class Meta:
+        app_label = "vis"
 
 
 class ResultsProbLSTM(models.Model):
@@ -140,4 +151,5 @@ class ResultsProbLSTM(models.Model):
     LTinc = models.FloatField(null=False)
 
     class Meta:
+        app_label = "vis"
         db_table = "results_prob_lstm"
