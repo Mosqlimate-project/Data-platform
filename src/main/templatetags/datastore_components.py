cc4124e066d4aecea2c4bcaf200182f56018514a
Luã Bida Vacaro <luabidaa@gmail.com>
2024-04-04 13:38:13 -0300

    chore(datastore): include episcanner's api url forms on /datastore/

--- src/main/templatetags/datastore_components.py
+++ src/main/templatetags/datastore_components.py
@@ -5,7 +5,7 @@ from main.utils import UFs
 register = template.Library()
 
 
-@register.inclusion_tag("main/components/datastore/infodengue.html")
+@register.inclusion_tag("datastore/components/infodengue.html")
 def infodengue():
     diseases = [
         ("dengue", "Dengue"),
@@ -18,13 +18,25 @@ def infodengue():
     return context
 
 
-@register.inclusion_tag("main/components/datastore/climate.html")
+@register.inclusion_tag("datastore/components/climate.html")
 def climate():
     context = {"UFs": UFs.items()}
     return context
 
 
-@register.inclusion_tag("main/components/datastore/mosquito.html")
+@register.inclusion_tag("datastore/components/mosquito.html")
 def mosquito():
     context = {}
+    return context
+
+
+@register.inclusion_tag("datastore/components/episcanner.html")
+def episcanner():
+    diseases = [
+        ("dengue", "Dengue"),
+        ("zika", "Zika"),
+        ("chik", "Chikungunya"),
+    ]
+
+    context = {"diseases": diseases, "UFs": UFs.items()}
     return context
