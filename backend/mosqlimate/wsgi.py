9a912a0e7222e04ee1e7c296265a299b4a1339f1
Luã Bida Vacaro <luabidaa@gmail.com>
2024-10-14 14:14:34 -0300

    add gcc+13 again

--- backend/mosqlimate/wsgi.py
+++ backend/mosqlimate/wsgi.py
@@ -10,9 +10,8 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from django.conf import settings
 from blacknoise import BlackNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mosqlimate.settings")
 
-application = BlackNoise(get_wsgi_application(), root=settings.STATIC_ROOT)
+application = BlackNoise(get_wsgi_application())
