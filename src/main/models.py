5551bd2cba6e786af1349ec69286874778115718
Luã Bida Vacaro <luabidaa@gmail.com>
2025-04-28 13:34:25 -0300

    feat(api): create APILog model to start logging the requests

---
+++ src/main/models.py
@@ -0,0 +1,55 @@
+from typing import Optional
+
+from django.db import models
+from django.http.request import HttpRequest
+from django.contrib.auth import get_user_model
+
+from mosqlimate.api import authorize
+
+
+User = get_user_model()
+
+
+class APILog(models.Model):
+    METHODS = [
+        ("GET", "get"),
+        ("POST", "post"),
+        ("PUT", "put"),
+        ("DELETE", "delete"),
+    ]
+
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        to=User, on_delete=models.CASCADE, related_name="logs", null=False
+    )
+    method = models.CharField(
+        max_length=10, choices=METHODS, blank=False, null=False
+    )
+    endpoint = models.CharField(max_length=512, null=False, blank=False)
+    params = models.JSONField()
+
+    @staticmethod
+    def from_request(request: HttpRequest, user: Optional[User] = None):
+        method = request.method
+
+        if not user:
+            user = authorize(request)
+
+        match method:
+            case "GET":
+                params = request.GET.dict()
+            case "POST":
+                params = request.POST.dict()
+            case "PUT" | "DELETE":
+                params = {}
+            case _:
+                raise NotImplementedError()
+
+        log = APILog(
+            user=user,
+            method=method,
+            endpoint=request.path,
+            params=params,
+        )
+        log.save()
+        return log
