f6d08d0e818bf1e5c82d3b690aa1955fe4b20126
Luã Bida Vacaro <luabidaa@gmail.com>
2025-05-27 16:58:48 -0300

    test with polygons

--- src/maps/urls.py
+++ src/maps/urls.py
@@ -1,5 +1,9 @@
-# from django.urls import path
-#
-# from . import views
-#
-# urlpatterns = []
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path("centroid/", views.centroid_map, name="centroid_map"),
+    path("multilayer/", views.multilayer_map, name="multilayer_map"),
+]
