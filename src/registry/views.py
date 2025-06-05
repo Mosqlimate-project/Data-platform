be9d342216c09ca7fc01bbb826bab9fe2ef51961
Luã Bida Vacaro <luabidaa@gmail.com>
2025-06-05 16:34:40 -0300

    feat(registry): include /registry/prediction/id/ page; update about and more

--- src/registry/views.py
+++ src/registry/views.py
@@ -1,10 +1,12 @@
-from typing import Any
+import json
 
-from django.http.response import JsonResponse
 from django.views import View
 from django.shortcuts import render, get_object_or_404
+from django.core.serializers.json import DjangoJSONEncoder
 
-from registry.models import Model, Tag
+from registry.models import Model, Prediction, Tag
+from vis.api import get_cases
+from vis.schema import HistoricoAlertaCasesIn
 
 
 class ModelView(View):
@@ -21,6 +23,33 @@ class ModelView(View):
         return render(request, self.template_name, context)
 
 
-def filter_predictions(
-    request, ids: list[int], filters: dict[str, Any]
-) -> JsonResponse: ...
+class PredictionView(View):
+    template_name = "registry/prediction.html"
+
+    def get(self, request, prediction_id: int):
+        prediction: Prediction = get_object_or_404(
+            Prediction, pk=prediction_id
+        )
+        data = list(prediction.data.values())
+        tags = [
+            Tag.objects.get(pk=id)
+            for id in Tag.get_tag_ids_from_model_id(prediction.model.id)
+        ]
+        payload = HistoricoAlertaCasesIn(
+            sprint=prediction.model.sprint,
+            disease=prediction.model.disease,
+            start=prediction.date_ini_prediction.date(),
+            end=prediction.date_end_prediction.date(),
+            adm_level=prediction.model.adm_level,
+            adm_1=prediction.adm_1.uf if prediction.adm_1 else None,
+            adm_2=prediction.adm_2.geocode if prediction.adm_2 else None,
+        )
+        cases = get_cases(request, payload)
+
+        context = {
+            "prediction": prediction,
+            "tags": tags,
+            "data": json.dumps(data, cls=DjangoJSONEncoder),
+            "cases": cases,
+        }
+        return render(request, self.template_name, context)
--- src/vis/api.py
+++ src/vis/api.py
@@ -2,82 +2,54 @@ from datetime import date
 from typing import Literal, List
 
 from ninja import Router
+from ninja.security import django_auth
 from django.db import IntegrityError
 from django.views.decorators.csrf import csrf_exempt
 
 from main.schema import UnprocessableContentSchema, ForbiddenSchema
 from users.auth import UidKeyAuth
-from .models import (
-    ResultsProbForecast,
-    GeoMacroSaude,
-    TotalCases,
-    TotalCases100kHab,
-)
-from .schema import ResultsProbForecastSchema, TotalCasesSchema
+from .dash.line_chart import hist_alerta_data
+from . import models
+from . import schema
 
 
 router = Router()
 uidkey_auth = UidKeyAuth()
 
 
-# @router.get(
-#     "/results-prob-forecast/",
-#     response={
-#         200: List[ResultsProbForecastSchema],
-#         422: UnprocessableContentSchema,
-#     },
-#     auth=django_auth,
-#     include_in_schema=True,
-# )
-# @paginate(PagesPagination)
-# def list_results_prob_forecast(
-#     request,
-#     disease: Optional[Literal["dengue", "zika", "chik"]],
-#     filters: ResultsProbForecastFilterSchema = Query(...),
-#     **kwargs,
-# ):
-#     if disease and disease not in ["dengue", "zika", "chik"]:
-#         return 422, {
-#             "message": "Incorrect disease, options: ['dengue', 'zika', 'chik']"
-#         }
-#
-#     try:
-#         date.fromisoformat(str(filters.date))
-#     except ValueError:
-#         return 422, {
-#             "message": "Incorrect date format, please use isoformat: YYYY-MM-dd"
-#         }
-#
-#     objs = filters.filter(
-#         ResultsProbForecast.objects.filter(disease=disease)
-#     ).order_by("-date")
-#
-#     res = [
-#         dict(
-#             disease=obj.disease,
-#             date=obj.date,
-#             geocode=int(obj.geocode.geocode),
-#             lower_2_5=obj.lower_2_5,
-#             lower_25=obj.lower_25,
-#             forecast=obj.forecast,
-#             upper_75=obj.upper_75,
-#             upper_97_5=obj.upper_97_5,
-#             prob_high=obj.prob_high,
-#             prob_low=obj.prob_low,
-#             high_threshold=obj.high_threshold,
-#             low_threshold=obj.low_threshold,
-#             high_incidence_threshold=obj.high_incidence_threshold,
-#             low_incidence_threshold=obj.low_incidence_threshold,
-#         )
-#         for obj in objs
-#     ]
-#
-#     return res
+@router.get(
+    "/cases/",
+    response=List[schema.HistoricoAlertaCases],
+    auth=django_auth,
+    include_in_schema=False,
+)
+def get_cases(request, payload: schema.HistoricoAlertaCasesIn):
+    df = hist_alerta_data(
+        sprint=payload.sprint,
+        disease=payload.disease,
+        start_window_date=payload.start,
+        end_window_date=payload.end,
+        adm_level=payload.adm_level,
+        adm_1=payload.adm_1,
+        adm_2=payload.adm_2,
+    )
+
+    if df.empty:
+        return list()
+
+    df = df.sort_values(by="date")
+
+    res = []
+    for _, row in df.iterrows():
+        res.append(
+            schema.HistoricoAlertaCases(date=row["date"], cases=row["target"])
+        )
+    return res
 
 
 @router.get(
     "/total-cases/",
-    response=List[TotalCasesSchema],
+    response=List[schema.TotalCasesSchema],
     # auth=uidkey_auth,
     include_in_schema=False,
 )
@@ -89,16 +61,16 @@ def get_total_cases(
     per_100k: bool = False,
 ):
     if per_100k:
-        total_cases = TotalCases100kHab
+        total_cases = models.TotalCases100kHab
     else:
-        total_cases = TotalCases
+        total_cases = models.TotalCases
     return total_cases.objects.filter(year=year, disease=disease)
 
 
 @router.post(
     "/results-prob-forecast/",
     response={
-        201: ResultsProbForecastSchema,
+        201: schema.ResultsProbForecastSchema,
         403: ForbiddenSchema,
         422: UnprocessableContentSchema,
     },
@@ -106,7 +78,9 @@ def get_total_cases(
     include_in_schema=True,
 )
 @csrf_exempt
-def post_results_prob_forecast(request, payload: ResultsProbForecastSchema):
+def post_results_prob_forecast(
+    request, payload: schema.ResultsProbForecastSchema
+):
     if payload.disease == "chik":
         payload.disease = "chikungunya"
 
@@ -126,11 +100,11 @@ def post_results_prob_forecast(request, payload: ResultsProbForecastSchema):
 
     data = payload.dict()
     try:
-        data["geocode"] = GeoMacroSaude.objects.get(pk=data["geocode"])
-    except GeoMacroSaude.DoesNotExist:
+        data["geocode"] = models.GeoMacroSaude.objects.get(pk=data["geocode"])
+    except models.GeoMacroSaude.DoesNotExist:
         return 422, {"message": f"Unknown geocode {data['geocode']}"}
 
-    obj = ResultsProbForecast(**data)
+    obj = models.ResultsProbForecast(**data)
 
     try:
         obj.save()
