850800c4e7de07980fbcb739ca080d372926376e
Luã Bida Vacaro <luabidaa@gmail.com>
2025-06-18 15:48:28 -0300

    include requests by user chart

--- src/main/APILog/api.py
+++ src/main/APILog/api.py
@@ -2,7 +2,7 @@ from dateutil import parser
 
 from ninja import Router
 from django.db.models.functions import TruncDate
-from django.db.models import Count
+from django.db.models import Count, F
 
 
 from main.models import APILog
@@ -21,4 +21,20 @@ def usage_by_day(request, endpoint: str, start: str):
         .values("day")
         .annotate(count=Count("id"))
         .order_by("day")
+    )
+
+
+@router.get(
+    "/usage-by-user/",
+    include_in_schema=False,
+)
+def usage_by_user(request, endpoint: str, start: str):
+    return list(
+        APILog.objects.filter(endpoint=endpoint, date__gte=parser.parse(start))
+        .values(
+            username=F("user__username"),
+            institution=F("user__author__institution"),
+        )
+        .annotate(count=Count("id"))
+        .order_by("-count")
     )
