a486769a10dbe93c35bbd37ad3ad658909e58762
Luã Bida Vacaro <luabidaa@gmail.com>
2026-01-27 12:33:19 -0300

    fix episcanner 404

--- backend/datastore/api.py
+++ backend/datastore/api.py
@@ -679,12 +679,7 @@ def get_episcanner(
         db.close()
 
     if df.empty:
-        return 404, {
-            "message": (
-                "No data for specific query "
-                f"(disease={disease}, uf={uf}, year={year})"
-            )
-        }
+        return 200, []
 
     # if geocode:
     #     df = df[df['geocode'].isin(geocode)]
