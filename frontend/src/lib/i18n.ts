57b0e532e8453b0e915e71e7280b9c03d26f160d
Luã Bida Vacaro <luabidaa@gmail.com>
2026-01-28 00:48:29 -0300

    chore: frontend changes on dashboard, navbar, home & others

--- frontend/src/lib/i18n.ts
+++ frontend/src/lib/i18n.ts
@@ -1,27 +1,30 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import HttpBackend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
 
-if (typeof window !== 'undefined') {
-  i18n
-    .use(HttpBackend)
-    .use(LanguageDetector)
-    .use(initReactI18next)
-    .init({
-      fallbackLng: 'en',
-      ns: ['common'],
-      defaultNS: 'common',
-      backend: {
-        loadPath: '/locales/{{lng}}/{{ns}}.json',
-      },
-      interpolation: {
-        escapeValue: false,
-      },
-      react: {
-        useSuspense: false,
-      },
-    });
-}
+import commonEn from '../../public/locales/en/common.json';
+import commonPt from '../../public/locales/pt/common.json';
+import commonEs from '../../public/locales/es/common.json';
+
+const resources = {
+  en: { common: commonEn },
+  pt: { common: commonPt },
+  es: { common: commonEs },
+};
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: 'en',
+    fallbackLng: 'en',
+    ns: ['common'],
+    defaultNS: 'common',
+    interpolation: {
+      escapeValue: false,
+    },
+    react: {
+      useSuspense: false
+    }
+  });
 
 export default i18n;
