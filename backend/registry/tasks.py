4f2ddb5c06a5ff0f36af22e3457d4d406ad10b0f
Luã Bida Vacaro <luabidaa@gmail.com>
2026-07-09 13:24:36 -0300

    fix(scores): include rule to calculate scores even for predictions >= 1 year ago if scores are None

--- backend/registry/tasks.py
+++ backend/registry/tasks.py
@@ -3,7 +3,7 @@ from dateutil.relativedelta import relativedelta
 from typing import Optional
 
 from django.core.cache import cache
-from django.db.models import Max
+from django.db.models import Subquery, OuterRef, Max, Q
 from django.utils import timezone
 
 from mosqlimate.celeryapp import app
@@ -16,14 +16,29 @@ from vis.utils import calculate_score
 def update_prediction_scores(prediction_ids: Optional[list[int]] = None):
     year_ago = timezone.now().date() - relativedelta(years=1)
 
+    latest = (
+        QuantitativePrediction.objects.filter(id=OuterRef("id"))
+        .annotate(max_date=Max("data__date"))
+        .values("max_date")
+    )
+
+    q = QuantitativePrediction.objects.annotate(end_date=Subquery(latest))
+
+    empty_scores = Q(
+        mae_score__isnull=True,
+        mse_score__isnull=True,
+        crps_score__isnull=True,
+        log_score__isnull=True,
+        interval_score__isnull=True,
+        wis_score__isnull=True,
+    )
+
+    filters = Q(end_date__gte=year_ago) | empty_scores
+
     if not prediction_ids:
-        predictions = QuantitativePrediction.objects.annotate(
-            end_date=Max("data__date")
-        ).filter(end_date__gte=year_ago)
+        predictions = q.filter(filters)
     else:
-        predictions = QuantitativePrediction.objects.annotate(
-            end_date=Max("data__date")
-        ).filter(id__in=prediction_ids, end_date__gte=year_ago)
+        predictions = q.filter(Q(id__in=prediction_ids) & filters)
 
     for prediction in predictions:
         try:
