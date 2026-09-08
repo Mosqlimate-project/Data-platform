15bae2b7463ddf0fd5fa7076101f99e2781ff85c
Luã Vacaro <luabidaa@gmail.com>
2026-09-08 09:12:38 -0300

    fix: remove ollama container & fetch swagger.json from prod instead (#548)

--- containers/mkdocs/entrypoint.sh
+++ containers/mkdocs/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
-wget -O ./docs/swagger.json https://api.mosqlimate.org/api/openapi.json
+mkdir -p ./docs
+for i in 1 2 3 4 5; do
+  if wget -O ./docs/swagger.json https://api.mosqlimate.org/api/openapi.json; then
+    break
+  fi
+  sleep 5
+done
 
 exec mkdocs serve --dev-addr=0.0.0.0:8000 --config-file ./mkdocs.yml
